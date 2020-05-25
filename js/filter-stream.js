0bc813a35ab02a175cfb05a395fbd966ab5d3491
Glitch (stupendous-frost-guilty) <none>
2020-05-25 08:45:15 +0000

    🍜🌒 Checkpoint ./js/filter-stream.js:15181578/33

--- js/filter-stream.js
+++ js/filter-stream.js
@@ -42,7 +42,7 @@ class FilterStream {
   setCanvasActiveLayer(activeLayer)
   {
     self.canvasActiveLayer = activeLayer;
-    console.log("Set canvas active layer: ", self.canvasActiveLayer)
+    console.log("Set canvas active layer: ", activeLayer)
   }
   
 }
